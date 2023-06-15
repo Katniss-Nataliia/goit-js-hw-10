f84bfac87193cb7210a81b4e3b09a5b87d16d4c5
Katniss-Nataliia <natko.boiko@gmail.com>
2023-06-14 21:22:17 -0600

    code

--- src/cat-api.js
+++ src/cat-api.js
@@ -12,20 +12,20 @@ export default {getBreeds};
 const breedSelect = document.querySelector(".breed-select");
 
 breedSelect.addEventListener("select", ()=>{
-    fetchBreeds()
+    getBreeds()
     .then((breed)=>renderBreedList(breed))
     .catch((error)=> console.error(error.message));
 })
 
-function fetchBreeds(){
-    return fetch("https://api.thecatapi.com/v1/breeds")
-    .then(response =>{
-        if(!response.ok){
-            throw new Error(response.message)
-        }
-        return response.json()
-    })
-}
+// function fetchBreeds(){
+//     return fetch("https://api.thecatapi.com/v1/breeds")
+//     .then(response =>{
+//         if(!response.ok){
+//             throw new Error(response.message)
+//         }
+//         return response.json()
+//     })
+// }
 
 function renderBreedList(breeds){
     const markUp = breeds.map((breed)=>{
